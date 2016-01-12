fd1a0e29c0de50ece3260ce973d3e7ad37fe942b
Iñaki Baz Castillo <ibc@aliax.net>
2016-01-12 17:25:05 +0100

    1.0.4: Ensure body is converted into string if it's an object.

--- dist/mimemessage.js
+++ dist/mimemessage.js
@@ -1,7 +1,7 @@
 /*
- * mimemessage v1.0.3
+ * mimemessage v1.0.4
  * MIME messages for JavaScript (RFC 2045 & 2046)
- * Copyright 2015 Iñaki Baz Castillo at eFace2Face, inc. (https://eface2face.com)
+ * Copyright 2015-2016 Iñaki Baz Castillo at eFace2Face, inc. (https://eface2face.com)
  * License MIT
  */
 
@@ -146,8 +146,10 @@ Entity.prototype.toString = function (options) {
 			raw += '--' + boundary + '\r\n' + this._body[i].toString();
 		}
 		raw += '\r\n--' + boundary + '--';
-	} else if (this._body) {
-		raw += this._body.toString();
+	} else if (typeof this._body === 'string') {
+		raw += this._body;
+	} else if (typeof this._body === 'object') {
+		raw += JSON.stringify(this._body);
 	}
 
 	return raw;
